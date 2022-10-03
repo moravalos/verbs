f5a45061320747bd9d7739f679f341c3d753bc17
Marcos Eduardo Mora Avalos <170628@utags.edu.mx>
2022-10-02 22:43:17 -0500

    First commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const showVerb = document.getElementById("showVerb");
+const showImage = document.getElementById("showImage");
+const showAudio = document.getElementById("showAudio");
+
+// Helper elements
+const next = document.getElementById("next");
+const verbsCounter = document.getElementById("verbs-counter");
+const allRightCounter = document.getElementById("all-right-answers");
+const verbsContainer = document.getElementById("verbs-container");
+// Answers
+const first = document.getElementById("first-verb");
+const second = document.getElementById("second-verb");
+const third = document.getElementById("third-verb");
+const fourth = document.getElementById("fourth-verb");
+
+// how many verbs we have
+const numberOfVerbs = verbs.length;
